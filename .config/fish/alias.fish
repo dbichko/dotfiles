28d1479b1536144edf75b2740713939e34844258
Dmitri Bichko <dbichko@gmail.com>
2016-12-10 16:17:16 -0500

    docker abbr

--- .config/fish/alias.fish
+++ .config/fish/alias.fish
@@ -10,4 +10,22 @@ abbr css 'sudo systemctl status'
 abbr csr 'sudo systemctl restart'
 abbr cse 'sudo systemctl enable'
 
+abbr startm 'sudo systemctl start lightdm'
+
+abbr gs 'git status'
+abbr ga 'git add'
+abbr gc 'git commit -a --allow-empty-message -m ""'
+abbr gu 'git pull'
+abbr gp 'git push'
+
+abbr dps 'docker ps'
+abbr dpsa 'docker ps -a'
+abbr dstop 'docker stop (docker ps -q)'
+abbr dclean 'docker rm -f (docker ps -a -q)'
+
 alias dotgit 'git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+
+function renew
+  sudo systemctl restart wpa_supplicant@wlp4s0
+  sudo systemctl restart systemd-networkd
+end
