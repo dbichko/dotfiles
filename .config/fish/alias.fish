153e2ee08b5535c8073d1c2705631618eb7c6c74
Dmitri Bichko <dbichko@gmail.com>
2016-12-10 20:56:14 -0500

    vagrant

--- .config/fish/alias.fish
+++ .config/fish/alias.fish
@@ -1,7 +1,11 @@
+## package ##
+
 abbr pacin 'sudo pacman -S'
 abbr pacup 'sudo pacman -Syu'
 abbr pacq 'pacman -Q'
 abbr pacs 'pacman -Ss'
+
+## systemd ##
 
 abbr cn 'sudo networkctl'
 abbr cj 'journalctl'
@@ -12,16 +16,35 @@ abbr cse 'sudo systemctl enable'
 
 abbr startm 'sudo systemctl start lightdm'
 
+## git ##
+
 abbr gs 'git status'
 abbr ga 'git add'
 abbr gc 'git commit -a --allow-empty-message -m ""'
 abbr gu 'git pull'
 abbr gp 'git push'
 
-abbr dps 'docker ps'
-abbr dpsa 'docker ps -a'
-abbr dstop 'docker stop (docker ps -q)'
-abbr dclean 'docker rm -f (docker ps -a -q)'
+## docker ##
+
+abbr dps    'docker ps'
+abbr dpsa   'docker ps -a'
+abbr dstop  'docker stop -t 0 (docker ps -q)'
+abbr dclean 'docker stop -t 0 (docker ps -q); docker rm -f (docker ps -a -q); docker rmi -f (docker images --filter dangling=true -q)'
+abbr dshell 'docker exec -ti (dc-last) sh'
+
+function di-last
+  docker images -q | head -n 1
+end
+
+function dc-last
+  docker ps -q | head -n 1
+end
+
+## vagrant ##
+
+abbr vclean 'vagrant destroy -f'
+
+## other ##
 
 alias dotgit 'git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
 
